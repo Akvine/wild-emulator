5f3b7120439777a770aa0dafa22d821702a2cbd7
akvine <redgooseman55@gmail.com>
2024-08-28 13:34:58 +0300

    git init

---
+++ src/test/java/ru/akvine/wild/emulator/WildEmulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.wild.emulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildEmulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
